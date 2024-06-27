2d5f5f501acc68000f2b4754299639634a566a2e
Upayan Mazumder <upayanm3@gmail.com>
2024-06-27 14:27:47 +0530

    🔴 404.tsx The file "404.tsx" has been deleted. The import of "SecondaryFooter" and its usage has been removed from the file.

--- qwik/src/routes/404.tsx
+++ qwik/src/routes/404.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import SecondaryFooter from "../components/secondary footer/secondary-footer";
 export default component$(() => {
   return (
     <>
@@ -9,7 +8,6 @@ export default component$(() => {
       <h2>404 - Not Found</h2>
       <p>The page you are looking for does not exist!</p>
     </div>
-    <SecondaryFooter/>
     </>
   );
 });
