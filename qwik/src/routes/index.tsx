02ac9765b47853660c225ea70c5d56a1003606bd
Upayan Mazumder <upayanm3@gmail.com>
2024-06-27 13:50:53 +0530

    🔴 qwik/src/components/hero/hero.module.css 🔴 qwik/src/components/hero/hero.tsx 🔴 qwik/src/routes/index.tsx

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import Timeline, { usePingData } from '../components/status/timeline/timeline';
-
+import Hero from "../components/hero/hero";
 export default component$(() => {
   return (
     <>
       <div role="presentation" class="ellipsis"></div>
+      <Hero/>
       <Timeline />
     </>
   );
