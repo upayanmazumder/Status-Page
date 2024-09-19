fcf9e44b76c28231bc38dfffaf986bb84e9af106
upayanmazumder <upayanm3@gmail.com>
2024-09-19 15:05:53 +0530

    Refactor header styling and add dynamic header links

--- qwik/src/components/header/header.tsx
+++ qwik/src/components/header/header.tsx
@@ -1,41 +1,28 @@
 import { component$ } from "@builder.io/qwik";
 import logo from "../../media/logo.png";
 import styles from "./header.module.css";
+import config from "../../data/config.json";
 
 export default component$(() => {
+  const { headerLinks, header, companyDetails } = config;
+
   return (
     <header class={styles.header}>
       <div class={["container", styles.wrapper]}>
-        <div class={styles.logo}>
-          <a href="https://upayan.space" title="Main Website" target="_blank">
-            <img src={logo} height={100} width={100} alt="Logo"></img>
-          </a>
-        </div>
+        <a href={companyDetails.website} title={companyDetails.companyName} target="_blank">
+          <div class={styles.branding}>
+            <div class={styles.logo}><img src={logo} height={100} width={100} alt="Logo"></img></div>
+            <h2 class={styles.title}>{header.title}</h2>
+          </div>
+        </a>
         <ul>
-          <li>
-            <a
-              href="https://github.com/upayanmazumder/Status-Page"
-              target="_blank"
-            >
-              Repository
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/upayanmazumder/Status-Page"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.paypal.com/paypalme/upayanmazumder"
-              target="_blank"
-            >
-              Support Me
-            </a>
-          </li>
+          {Object.entries(headerLinks).map(([name, url]) => (
+            <li key={name}>
+              <a href={url} target="_blank">
+                {name}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </header>
